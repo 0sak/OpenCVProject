a95c378eb758159fd3aed1e9b983773b761b9d80
f0gel <kolo414141@gmail.com>
2021-11-16 12:10:16 +0100

    Added folder for test images, Looping through those images and applying several filter (Grayscaling, GausiannBlur, Threshold, Sobel_x, Sobel_y, Opening.

--- OpenCVTest/OpenCVTest.cpp
+++ OpenCVTest/OpenCVTest.cpp
@@ -1,24 +1,54 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<string>
+#include <opencv2/imgproc/imgproc.hpp>
 
-using namespace cv;
-using namespace std;
 
 int main(int argc, char** argv)
 {
-    // Read the image file
-    Mat image = imread("auto.png");
-    // Check for failure
-    if (image.empty())
-    {
-        cout << "Image Not Found!!!" << endl;
-        cin.get(); //wait for any key press
-        return -1;
-    }
-    // Show our image inside a window.
-    imshow("OpenCVTest", image);
+    for (int i = 1; i < 4; i++) {
+        cv::Mat imageGrey;
+        cv::Mat image;
+        cv::Mat imageBlurred;
+        cv::Mat imageThresh;
+        cv::Mat imageConcat;
+        cv::Mat sobel_x, sobel_y;
+        cv::Mat sobelResult;
+        cv::Mat erodeResult; 
+        cv::Mat dilateResult;
 
-    // Wait for any keystroke in the window
-    waitKey(0);
+        int ddepth = CV_32F;
+
+        std::string imageToRead = "images/";
+
+        imageToRead += std::to_string(i);
+        imageToRead += ".png";
+
+        std::cout << imageToRead << std::endl;
+
+        image = cv::imread(imageToRead);
+
+        if (image.empty()) {
+            std::cout << "Image not Found !!!" << std::endl;
+            std::cin.get();
+        }
+
+        cv::cvtColor(image, imageGrey, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(imageGrey, imageBlurred, cv::Size(3, 3), 0);
+        cv::threshold(imageBlurred, imageThresh, 127, 255, cv::THRESH_BINARY);
+        cv::Sobel(imageThresh, sobel_x, ddepth, 1, 0, 3, cv::BORDER_DEFAULT);
+        cv::Sobel(imageThresh, sobel_y, ddepth, 0, 1, 3, cv::BORDER_DEFAULT);
+        cv::magnitude(sobel_x, sobel_y, sobelResult);
+        
+        cv::erode(sobelResult, erodeResult, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 1)));
+        cv::dilate(erodeResult, dilateResult, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 1)));
+        
+        cv::hconcat(dilateResult, sobelResult, imageConcat);
+
+        cv::imshow(imageToRead, imageConcat);
+
+        cv::waitKey(0);
+    }
+
     return 0;
 }
